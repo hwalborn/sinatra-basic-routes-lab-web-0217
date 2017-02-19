6d04e1ee0048ea722e01934fafb92a4aaa3b8bb2
hwalborn <hwalborn@gmail.com>
2017-02-18 19:19:43 -0500

    donezo

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Holt\n"
+  end
+
+  get '/hometown' do
+    "My hometown is Brooklyn, NY\n"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is The Macarena"
+  end
 end
